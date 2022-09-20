360ef174b7a64c7effac2c13bd47a760eef6cf08
Takhirzhon <tashmatovtahir@gmail.com>
2022-09-20 14:45:28 +0600

    Time limit exceeded

---
+++ week two/task/Summation.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+class Summation {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int testCase = scan.nextInt();
+        long num1;
+        long num2;
+
+        while (0 <= testCase) {
+            num1 = scan.nextLong();
+            num2 = scan.nextLong();
+
+            if (num1 == 1) {
+                num1++;
+            }
+            if ((num1 - num2) <= 1000000000) {
+
+                for (long j = num1; j <= num2; j++) {
+
+                    int count = 0;
+
+                    for (long k = 2; k <= j / 2; k++) {
+                        if (j % k == 0) {
+                            count = 1;
+                            break;
+                        }
+                    }
+                    if (count == 0) {
+                        System.out.println(j);
+                    }
+
+                }
+            }
+        }
+
+
+    }
+}
