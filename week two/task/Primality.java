5d980f7846fc70431b608ec4be6be00aca4e1665
Takhirzhon <tashmatovtahir@gmail.com>
2022-09-24 00:15:27 +0600

    Updated

--- week two/task/Primality.java
+++ week two/task/Primality.java
@@ -4,10 +4,10 @@ public class Primality {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        int test = scan.nextInt();
+        long test = scan.nextInt();
         while (test-- > 0) {
             boolean chk = true;
-            int num = scan.nextInt();
+            long num = scan.nextInt();
             if (num == 1) {
                 System.out.println("no");
                 continue;
@@ -16,7 +16,7 @@ public class Primality {
                 System.out.println("yes");
                 continue;
             }
-            for (int k = 2; k < num/2 + 1; k++) {
+            for (long k = 2; k < num/2 + 1; k++) {
                 if (num % k == 0) {
                     System.out.println("no");
                     chk = false;
