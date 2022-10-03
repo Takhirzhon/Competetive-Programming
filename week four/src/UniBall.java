075d762a952841eb4452a75400dbae6ede752cf5
Takhirzhon <tashmatovtahir@gmail.com>
2022-10-03 18:28:52 +0600

    Didn't solved yet

---
+++ week four/src/UniBall.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class UniBall {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int numOfBoys, numOfGirls;
+        int counter = 0;
+        numOfBoys = scan.nextInt(); ;
+        int a [numOfBoys];
+
+    }
+}
