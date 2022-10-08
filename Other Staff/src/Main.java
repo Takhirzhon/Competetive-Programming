86a757ea0cb0b0f3f4d7ee8dce73e53eaaf5ad19
Takhirzhon <tashmatovtahir@gmail.com>
2022-10-08 12:55:03 +0600

    Working on project

---
+++ Other Staff/src/Main.java
@@ -0,0 +1,19 @@
+class Student {
+    public  int studDoc;
+    public  String name;
+    public String surname;
+    public int yearOfStudy;
+    public double averageMath;
+    public double averageEco;
+    public double averageLang;
+}
+
+class StudentTest{
+
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
