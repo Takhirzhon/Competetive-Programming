9e60b571561d326fca19092a85dfdcceef12e9e5
Takhirzhon <tashmatovtahir@gmail.com>
2022-10-13 23:03:29 +0600

    Working on

---
+++ week #1/src/Subset.java
@@ -0,0 +1,16 @@
+import java.sql.Array;
+import java.util.Scanner;
+
+public class Subset {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int test = scan.nextInt();
+        int [] array = new int[args.length];
+
+
+        for (int i = 0; i < test; i++) {
+           // array
+        }
+    }
+}
