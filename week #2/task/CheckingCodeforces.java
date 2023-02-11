b594a982812b63225514ef5ea04b1327cdfac2a3
Takhirzhon <tashmatovtahir@gmail.com>
2023-02-11 23:33:18 +0600

    Solved

--- week #2/task/CheckingCodeforces.java
+++ week #2/task/CheckingCodeforces.java
@@ -9,7 +9,6 @@ public class CheckingCodeforces {
             if("codeforces".indexOf(letter) != -1) {
                 System.out.println("YES");
             }
-
             else {
                 System.out.println("NO");
             }
