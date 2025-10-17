e9c446f99bce0a78f74456bea86e89e69a817c5a
Tokhirzhon Tashmatov <tashmatovtahir@gmail.com>
2025-10-18 02:42:45 +0600

    Done

---
+++ Leetcode/src/comparenums.py
@@ -0,0 +1,18 @@
+#This code below is O(n^2)
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] == nums[j]:
+                    return True
+        return False
+
+#Better approach where O(N)
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        seen = {}
+        for n in nums:
+            if n in seen:
+                return True
+            seen[n] = True
+        return False
