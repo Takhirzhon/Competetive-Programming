8ffabc70bb89bb3ec63b66d022330a639d00238e
Tokhirzhon Tashmatov <tashmatovtahir@gmail.com>
2026-01-08 08:58:18 +0600

    Update

--- Leetcode/src/robII.py
+++ Leetcode/src/robII.py
@@ -21,4 +21,23 @@ class Solution:
 
         return max(case1, case2)
 
- 
+
+# Version with the Function
+
+#  class Solution:
+#     def rob(self, nums: List[int]) -> int:
+#         n = len(nums)
+#         if n == 1:
+#             return nums[0]
+#         if n == 0:
+#             return 0
+
+
+#         def robbery(nums):
+#             rob1, rob2 = 0, 0
+#             for n in nums:
+#                 temp = max(rob1 + n, rob2)
+#                 rob1 = rob2
+#                 rob2 = temp
+#             return rob2
+#         return max(robbery(nums[:-1]), robbery(nums[1:]))
